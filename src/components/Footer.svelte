d8e1b830f974d9b86f56fcf1fc39c1a27328fd0b
xvargr <targenta650@gmail.com>
2023-01-24 23:46:06 +0800

    layout and welcome screen changes

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -7,7 +7,10 @@
   footer {
     height: calculated.$footer-height;
     background-color: colors.$darkest;
+    // background-color: red;
     color: colors.$lighter;
     padding: 1rem;
+    // margin-left: 5rem;
+    // z-index: 50;
   }
 </style>
