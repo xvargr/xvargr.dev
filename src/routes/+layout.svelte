db480af62870d9a010990bc6ef4866e5f27b73fa
xvargr <targenta650@gmail.com>
2023-01-27 23:23:33 +0800

    canvas nav changes, layout changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import "../styles/reset.scss";
   import "../styles/global.scss";
 
-  import Footer from "../components/Footer.svelte";
+  // import Footer from "../components/Footer.svelte";
   import CanvasNav from "../components/CanvasNav.svelte";
 </script>
 
@@ -14,29 +14,38 @@
 <div class="content-wrapper" />
 
 <CanvasNav />
-<main>
-  <slot />
-</main>
-<Footer />
+<slot />
 
+<!-- <Footer /> -->
 <style lang="scss">
   .content-wrapper {
     display: flex;
   }
 
-  main {
-    display: flex;
-    flex-direction: column;
-    min-height: 100vh;
-  }
+  // main {
+  //   // display: flex;
+  //   // flex-direction: column;
+  //   // min-height: 100vh;
+  //   // width: 200px;
+  // }
 
-  @media (min-width: 1536px) {
-    main {
-      max-width: 85rem;
+  // @media (min-width: 1024px) {
+  //   main {
+  //     max-width: 70rem;
 
-      margin: auto;
-      left: 0;
-      right: 0;
-    }
-  }
+  //     margin: auto;
+  //     left: 0;
+  //     right: 0;
+  //   }
+  // }
+
+  // @media (min-width: 1536px) {
+  //   main {
+  //     max-width: 85rem;
+
+  //     margin: auto;
+  //     left: 0;
+  //     right: 0;
+  //   }
+  // }
 </style>
