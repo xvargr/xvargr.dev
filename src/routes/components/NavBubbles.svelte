504ff1a2f168b860c66097c84d45128ee7161ecf
xvargr <targenta650@gmail.com>
2023-02-20 13:35:15 +0800

    add navigation item selection indicatior, loading modal firefox bug workaround

--- src/routes/components/NavBubbles.svelte
+++ src/routes/components/NavBubbles.svelte
@@ -1,41 +1,63 @@
 <script>
-  import { onMount } from "svelte";
-  import { themeColor } from "../../stores/states";
+  import { fade } from "svelte/transition";
+  import { page } from "$app/stores";
 
   import Mail from "./svg/MailSVG.svelte";
   import HomeSVG from "./svg/HomeSVG.svelte";
   import Briefcase from "./svg/BriefcaseSVG.svelte";
   import CodeSvg from "./svg/CodeSVG.svelte";
-
-  onMount(() => {
-    const navItems = document.querySelectorAll(".nav-item");
-
-    navItems.forEach((element) => {
-      element.style.backgroundColor = $themeColor.background;
-      element.style.color = $themeColor.text;
-    });
-  });
 </script>
 
 <nav>
-  <a class="nav-item" href="/" title="home">
-    <HomeSVG />
+  <a class="nav-item" href="/" title="home" class:selected={$page.route.id === "/"}>
+    {#if $page.route.id === "/"}
+      <span in:fade>home</span>
+    {:else}
+      <HomeSVG />
+    {/if}
   </a>
-  <a class="nav-item" href="/projects" title="projects">
-    <CodeSvg />
+  <a
+    class="nav-item"
+    href="/projects"
+    title="projects"
+    class:selected={$page.route.id === "/projects"}
+  >
+    {#if $page.route.id === "/projects"}
+      <span in:fade>projects</span>
+    {:else}
+      <CodeSvg />
+    {/if}
   </a>
-  <a class="nav-item" href="/experience" title="experience">
-    <Briefcase />
+  <a
+    class="nav-item"
+    href="/experience"
+    title="experience"
+    class:selected={$page.route.id === "/experience"}
+  >
+    {#if $page.route.id === "/experience"}
+      <span in:fade>experience</span>
+    {:else}
+      <Briefcase />
+    {/if}
   </a>
-  <a class="nav-item" href="/contact" title="contact and CV">
-    <span>
-      <Mail />
-    </span>
+  <a
+    class="nav-item"
+    href="/contact"
+    title="contact and CV"
+    class:selected={$page.route.id === "/contact"}
+  >
+    {#if $page.route.id === "/contact"}
+      <span in:fade>contact</span>
+    {:else}
+      <Mail transition />
+    {/if}
   </a>
 </nav>
 
 <style lang="scss">
   @use "../../styles/colors.scss";
+
+  $nominal-item-dimension: 3rem;
 
   nav {
     position: absolute;
@@ -46,19 +68,29 @@
     z-index: 10;
 
     > .nav-item {
+      display: flex;
+      justify-content: center;
+      align-items: center;
       position: relative;
       height: 3rem;
       width: 3rem;
       margin: 0.4rem;
+      font-weight: 600;
       border-radius: 99rem;
       padding: 0.6rem;
-      color: colors.$grey;
+      color: var(--text-color);
+      background-color: var(--background-color);
       box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
-      transition: background-color 100ms;
+      text-decoration: none;
+      transition: background-color 100ms, width ease-in-out 200ms;
     }
 
     > .nav-item:hover {
       background-color: var(--highlight-color) !important; // js assign highlight bodge
+    }
+
+    > .nav-item.selected {
+      width: 7rem;
     }
   }
 
@@ -79,11 +111,11 @@
 
   @media (min-width: 768px) {
     nav {
-      width: fit-content;
+      width: 8.75rem;
       height: fit-content;
       margin-top: 0.4rem;
       top: 0;
-      right: -3rem;
+      right: -6.5rem;
       flex-direction: column;
       justify-content: start;
     }
